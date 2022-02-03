2cee9fc89c91cb36c68adae8d70168cf0025c8ec
Lillian Paris <lillian.paris529@gmail.com>
2022-02-03 10:23:21 -0700

    Added

--- src/Components/Carousel/carousel.jsx
+++ src/Components/Carousel/carousel.jsx
@@ -28,6 +28,7 @@ componentDidMount() {
     document.addEventListener('DOMContentLoaded', function() {
         var elems = document.querySelectorAll('.carousel');
         var instances = M.Carousel.init(elems);
+        instances()
       });
     
 
