cbc6ba6ecf4f6c02866bd1678624486d056a2c90
Lillian Paris <lillian.paris529@gmail.com>
2022-02-03 09:44:21 -0700

    Added

--- src/Components/Carousel/carousel.jsx
+++ src/Components/Carousel/carousel.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "../Carousel/style.css"
 import 'materialize-css';
-import M from "materialize-css";
+// import M from "materialize-css";
 import Chai from "../Images/chailogo.png";
 import css from "../Images/csslogo.png";
 import express from "../Images/expresslogo.png";
@@ -23,32 +23,32 @@ import { Row, Container } from "react-bootstrap";
 
 
 export class Caro extends Component {
-  componentDidMount() {
-    document.addEventListener("DOMContentLoaded", function () {
-      var elems = document.querySelectorAll(".carousel");
-      M.Carousel.init(elems, {
-        indicators: true,
-        duration: 300,
-        autoPlay: true
-      });
-      let indicatorItems = document.querySelectorAll('.carousel .indicator-item'),
-        slideTime = 1000,
-        activeClass = "active";
+//   componentDidMount() {
+//     document.addEventListener("DOMContentLoaded", function () {
+//       var elems = document.querySelectorAll(".carousel");
+//       M.Carousel.init(elems, {
+//         indicators: true,
+//         duration: 300,
+//         autoPlay: true
+//       });
+//       let indicatorItems = document.querySelectorAll('.carousel .indicator-item'),
+//         slideTime = 1000,
+//         activeClass = "active";
 
-      setInterval(() => {
-        indicatorItems.forEach(el => {
-          if (el.classList.contains(activeClass)) {
-            M = el.nextElementSibling;
-            if (M == null) {
-              indicatorItems[0].click();
-            } else {
-              M.click()
-            }
-          }
-        });
-      }, slideTime);
-    });
-  }
+//       setInterval(() => {
+//         indicatorItems.forEach(el => {
+//           if (el.classList.contains(activeClass)) {
+//             M = el.nextElementSibling;
+//             if (M == null) {
+//               indicatorItems[0].click();
+//             } else {
+//               M.click()
+//             }
+//           }
+//         });
+//       }, slideTime);
+//     });
+//   }
 
   render() {
     return (
